3f5ef14c2149ce5d98f689509eb33167f8513d7c
lvheng <1834805770@qq.com>
2025-06-10 17:51:41 +0800

    feat: shadcn-vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
@@ -11,8 +12,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/"><Button>首页</Button></RouterLink>
+        <RouterLink to="/about"><Button>关于</Button></RouterLink>
+        
       </nav>
     </div>
   </header>
