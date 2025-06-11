d7e10c8ed698745e86794aaaa03093797ae88fdd
lvheng <1834805770@qq.com>
2025-06-11 14:07:18 +0800

    feat: unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,23 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
     tailwindcss(),
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/],
+      imports: [ 'vue', 'vue-router'],
+      dts: 'src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true, // <-- this
+      },
+    }),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
